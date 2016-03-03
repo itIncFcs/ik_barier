261164b8cb2bc7f5def77928b3d2dba7b0c5a04f
AndrKlesh <andr_mast@mail.ru>
2016-03-03 21:34:43 +0600

    Add SerialPort and BarrierListener

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
+import jssc.SerialPortException;
+
+import java.util.Scanner;
+
 /**
- * Created by Вадим on 16.02.2016.
+ * Created by пїЅпїЅпїЅпїЅпїЅ on 16.02.2016.
  */
 public class Main {
+    static SerialChannelToController serialChannelToController = null;
     public static void main(String[] args){
-        
+
+        try {
+            serialChannelToController = new SerialChannelToController("/dev/ttyUSB0");
+            BarrierListener barrierListener = new BarrierListener() {
+                @Override
+                public void barrierBroken() {
+                    System.out.println("barrierBroken");
+                    Scanner in = new Scanner(System.in);
+                    in.next();
+                    serialChannelToController.send(serialChannelToController.OPEN_BARRIER);
+                }
+            };
+            serialChannelToController.setBarrierListener(barrierListener);
+            serialChannelToController.open();
+            serialChannelToController.startReceive();
+
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                serialChannelToController.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
