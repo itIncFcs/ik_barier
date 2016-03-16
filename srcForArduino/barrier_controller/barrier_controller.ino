42c2c8a8ac5a997c0992e8dc53cd9a9dc441f6ae
AndrKlesh <andr_mast@mail.ru>
2016-03-16 19:59:40 +0600

    correct code for arduino

--- srcForArduino/barrier_controller/barrier_controller.ino
+++ srcForArduino/barrier_controller/barrier_controller.ino
@@ -1,27 +1,31 @@
-byte ledPin = 13;
-byte inputPin = 2;
+byte ledPin = 13;         //для светодиода
+byte barrierOpenPin = 12; //открытие\закрытие шлагбаума
+byte inputPin = 2;	  //вход датчиков
 int delayBarrier = 6000;
 boolean barrierBroken = false;
 byte incomingByte;
 
 void setup() {
-  pinMode(ledPin, OUTPUT); //инициализация 13 пина как порт вывода
-  pinMode(inputPin, INPUT);//инициализация 2 пина как порт ввода
-  Serial.begin(9600);      //установка скорости последовательного порта
+  pinMode(ledPin, OUTPUT);         //инициализация 13 пина как порт вывода для светодиода
+  pinMode(barrierOpenPin, OUTPUT); //инициализация 12 пина как порт вывода для открытия шлагбаума
+  pinMode(inputPin, INPUT);        //инициализация 2 пина как порт ввода
+  Serial.begin(9600);              //установка скорости последовательного порта
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    incomingByte = Serial.read(); //считывание команды с сервера/ПК
+    incomingByte = Serial.read();         //считывание команды с сервера/ПК
     if(incomingByte == '2'){
-      digitalWrite(ledPin, HIGH); //включение светодиода   
+      digitalWrite(ledPin, HIGH);         //включение светодиода 
+      digitalWrite(barrierOpenPin, HIGH); //открытие шлагбаума   
 
       do{
-        delay(delayBarrier);              //задержка на проезд автомобиля (необходимо корректировать)
+        delay(delayBarrier);       //задержка на проезд автомобиля (необходимо корректировать)
       }
-      while(isBarrierBroken());   //цикл пока барьер разорван
+      while(isBarrierBroken());    //цикл пока барьер разорван
       delay(delayBarrier);
-      digitalWrite(ledPin, LOW);
+      digitalWrite(ledPin, LOW);       
+      digitalWrite(barrierOpenPin, LOW); //Закрытие шлагбаума
     }
   }
 
@@ -34,9 +38,9 @@ void loop() {
 }
 
 boolean isBarrierBroken(){
-  boolean temp = (digitalRead(inputPin) == LOW);//High если барьер нарушен
-  delay(10);                                    //Программная защита 
-  return temp & (digitalRead(inputPin) == LOW); //от дребезга контактов
+  boolean temp = (digitalRead(inputPin) == HIGH);//High если барьер нарушен
+  delay(10);                                     //Программная защита 
+  return temp & (digitalRead(inputPin) == HIGH); //от дребезга контактов
 }
 
 
